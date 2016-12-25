614562bda6ed969cde675407502b0ec0479fbe5f
Indrit Bashkimi <indrit.bashkimi@gmail.com>
2016-12-25 12:10:17 +0100

    Add BootCompleteReceiver. Not working yet..

---
+++ app/src/main/java/it/ibashkimi/lockscheduler/receivers/BootCompleteReceiver.java
@@ -0,0 +1,34 @@
+package it.ibashkimi.lockscheduler.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+import it.ibashkimi.lockscheduler.Profiles;
+import it.ibashkimi.lockscheduler.api.GeofenceApiHelper;
+import it.ibashkimi.lockscheduler.api.GoogleApiHelper;
+import it.ibashkimi.lockscheduler.domain.Profile;
+
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "BootCompleteReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive");
+        ArrayList<Profile> profiles = Profiles.restoreProfiles(context);
+        if (profiles.size() > 0) {
+            Log.d(TAG, "onReceive: initGeofencing");
+            for (Profile profile: profiles)
+                profile.setEntered(false);
+            Profiles.saveProfiles(context, profiles);
+            GoogleApiHelper googleApiHelper = new GoogleApiHelper(context);
+            GeofenceApiHelper geofenceApiHelper = new GeofenceApiHelper(context, googleApiHelper);
+            geofenceApiHelper.initGeofences();
+        }
+    }
+}
