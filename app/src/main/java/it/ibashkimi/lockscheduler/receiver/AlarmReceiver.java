2ff3cb22b35f175679e47893c274b76a771c557a
Indrit Bashkimi <indrit.bashkimi@gmail.com>
2017-05-21 03:24:34 +0200

    Implemented boot delay.

--- app/src/main/java/it/ibashkimi/lockscheduler/receiver/AlarmReceiver.java
+++ app/src/main/java/it/ibashkimi/lockscheduler/receiver/AlarmReceiver.java
@@ -4,15 +4,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.PowerManager;
-import android.util.Log;
 import android.widget.Toast;
 
 import it.ibashkimi.lockscheduler.model.scheduler.ProfileScheduler;
 
-
 public class AlarmReceiver extends BroadcastReceiver {
-
-    private static final String TAG = "AlarmReceiver";
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -20,11 +16,15 @@ public class AlarmReceiver extends BroadcastReceiver {
         PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "");
         wl.acquire();
 
-        // Put here YOUR code.
         Toast.makeText(context, "AlarmReceiver!", Toast.LENGTH_LONG).show();
-
-        String profileId = intent.getStringExtra("profileId");
-        ProfileScheduler.Companion.getInstance().getTimeHandler().onAlarm(profileId);
+        if (intent.getExtras().containsKey("profileId")) {
+            String profileId = intent.getStringExtra("profileId");
+            ProfileScheduler.Companion.getInstance().getTimeHandler().onAlarm(profileId);
+        } else if (intent.getExtras().containsKey("boot")) {
+            ProfileScheduler.Companion.getInstance().init();
+        } else {
+            throw new IllegalArgumentException("Don't know what to do with this intent: " + intent + ".");
+        }
 
         wl.release();
     }
--- app/src/main/java/it/ibashkimi/lockscheduler/receiver/BootCompletedReceiver.java
+++ app/src/main/java/it/ibashkimi/lockscheduler/receiver/BootCompletedReceiver.java
@@ -1,10 +1,15 @@
 package it.ibashkimi.lockscheduler.receiver;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.util.Log;
+import android.widget.Toast;
 
+import it.ibashkimi.lockscheduler.App;
 import it.ibashkimi.lockscheduler.model.scheduler.ProfileScheduler;
 
 
@@ -15,6 +20,22 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "onReceive");
-        ProfileScheduler.Companion.getInstance().init();
+        SharedPreferences sharedPreferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+        long delay = Long.parseLong(sharedPreferences.getString("boot_delay", "0"));
+        if (delay < 0)
+            throw new IllegalArgumentException("Delay cannot be negative. Delay = " + delay + ".");
+        if (delay == 0)
+            ProfileScheduler.Companion.getInstance().init();
+        else {
+            Toast.makeText(context, "Setting alarm. Delay = " + delay, Toast.LENGTH_SHORT).show();
+            App.getLockManager().resetPassword();
+            AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            Intent alarmIntent = new Intent(context, AlarmReceiver.class);
+            alarmIntent.putExtra("boot", "boot");
+            PendingIntent pi = PendingIntent.getBroadcast(context, 1, alarmIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+            long now = System.currentTimeMillis();
+            long nextAlarm = now + delay;
+            am.set(AlarmManager.RTC_WAKEUP, nextAlarm, pi);
+        }
     }
 }
