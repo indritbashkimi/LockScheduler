82e831bde6c52e00dd8f756521462607afbf3fa7
Indrit Bashkimi <indrit.bashkimi@gmail.com>
2019-06-07 20:39:58 +0200

    Refresh about section.

--- app/src/main/java/com/ibashkimi/lockscheduler/about/AboutFragment.kt
+++ app/src/main/java/com/ibashkimi/lockscheduler/about/AboutFragment.kt
@@ -24,11 +24,6 @@ class AboutFragment : Fragment(), View.OnClickListener {
         view.findViewById<View>(R.id.feedback).setOnClickListener(this)
         view.findViewById<View>(R.id.privacy_policy).setOnClickListener(this)
         view.findViewById<View>(R.id.licenses).setOnClickListener(this)
-        view.findViewById<View>(R.id.email).setOnClickListener(this)
-        view.findViewById<View>(R.id.facebook).setOnClickListener(this)
-        view.findViewById<View>(R.id.google_plus).setOnClickListener(this)
-        view.findViewById<View>(R.id.twitter).setOnClickListener(this)
-        view.findViewById<View>(R.id.github).setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
@@ -38,11 +33,6 @@ class AboutFragment : Fragment(), View.OnClickListener {
             R.id.feedback -> PlatformUtils.sendFeedback(requireContext())
             R.id.privacy_policy -> Toast.makeText(context, "Not implemented yet", Toast.LENGTH_SHORT).show()
             R.id.licenses -> navController.navigate(R.id.action_about_to_licenses)
-            R.id.email -> PlatformUtils.sendFeedback(requireContext())
-            R.id.facebook -> openUrl(R.string.social_facebook)
-            R.id.twitter -> openUrl(R.string.social_twitter)
-            R.id.google_plus -> openUrl(R.string.social_google_plus)
-            R.id.github -> openUrl(R.string.social_github)
         }
     }
 
@@ -54,6 +44,6 @@ class AboutFragment : Fragment(), View.OnClickListener {
         val builder = CustomTabsIntent.Builder()
         //builder.setToolbarColor(ContextCompat.getColor(this, R.color.primary));
         val customTabsIntent = builder.build()
-        customTabsIntent.launchUrl(context!!, Uri.parse(url))
+        customTabsIntent.launchUrl(requireContext(), Uri.parse(url))
     }
 }
